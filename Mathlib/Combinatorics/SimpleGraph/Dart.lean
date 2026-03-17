d978ba5d1f5241a9b29a7103db25b1976d4b2267
Jun2M <rnjsgurwns27@gmail.com>
2026-03-16 20:46:21 -0400

    wip

--- Mathlib/Combinatorics/SimpleGraph/Dart.lean
+++ Mathlib/Combinatorics/SimpleGraph/Dart.lean
@@ -5,108 +5,75 @@ Authors: Kyle Miller
 -/
 module
 
-public import Mathlib.Combinatorics.SimpleGraph.Basic
-public import Mathlib.Data.Fintype.Sigma
+public import Mathlib.Combinatorics.HasDart.Basic
+public import Mathlib.Combinatorics.SimpleGraph.HasDart
 
 /-!
-# Darts in graphs
+# Darts in simple graphs
 
-A `Dart` or half-edge or bond in a graph is an ordered pair of adjacent vertices, regarded as an
-oriented edge. This file defines darts and proves some of their basic properties.
+This files gives simple-graph-specific properties of darts.
 -/
 
 @[expose] public section
 
-namespace SimpleGraph
+namespace HasDart
 
-variable {V : Type*} (G : SimpleGraph V)
-
-/-- A `Dart` is an oriented edge, implemented as an ordered pair of adjacent vertices.
-This terminology comes from combinatorial maps, and they are also known as "half-edges"
-or "bonds." -/
-structure Dart extends V × V where
-  adj : G.Adj fst snd
-  deriving DecidableEq
-
-initialize_simps_projections Dart (+toProd, -fst, -snd)
-
-attribute [simp] Dart.adj
-
-variable {G}
-
-theorem Dart.ext_iff (d₁ d₂ : G.Dart) : d₁ = d₂ ↔ d₁.toProd = d₂.toProd := by
-  cases d₁; cases d₂; simp
-
-@[ext]
-theorem Dart.ext (d₁ d₂ : G.Dart) (h : d₁.toProd = d₂.toProd) : d₁ = d₂ :=
-  (Dart.ext_iff d₁ d₂).mpr h
-
-@[simp]
-theorem Dart.fst_ne_snd (d : G.Dart) : d.fst ≠ d.snd :=
-  fun h ↦ G.irrefl (h ▸ d.adj)
-
-@[simp]
-theorem Dart.snd_ne_fst (d : G.Dart) : d.snd ≠ d.fst :=
-  fun h ↦ G.irrefl (h ▸ d.adj)
-
-theorem Dart.toProd_injective : Function.Injective (Dart.toProd : G.Dart → V × V) :=
-  Dart.ext
-
-instance Dart.fintype [Fintype V] [DecidableRel G.Adj] : Fintype G.Dart :=
-  Fintype.ofEquiv (Σ v, G.neighborSet v)
-    { toFun := fun s => ⟨(s.fst, s.snd), s.snd.property⟩
-      invFun := fun d => ⟨d.fst, d.snd, d.adj⟩ }
+variable {V : Type*} {G : SimpleGraph V}
 
 /-- The edge associated to the dart. -/
-def Dart.edge (d : G.Dart) : Sym2 V := s(d.fst, d.snd)
+def prodDart.edge (d : prodDart G) : Sym2 V := s(d.fst, d.snd)
 
 @[simp]
-theorem Dart.edge_mk {p : V × V} (h : G.Adj p.1 p.2) : (Dart.mk p h).edge = s(p.1, p.2) :=
+theorem prodDart.edge_mk {p : V × V} (h : G.Adj p.1 p.2) : (prodDart.mk p h).edge = s(p.1, p.2) :=
   rfl
 
 @[simp]
-theorem Dart.edge_mem (d : G.Dart) : d.edge ∈ G.edgeSet :=
-  d.adj
+theorem prodDart.edge_mem (d : prodDart G) : d.edge ∈ G.edgeSet :=
+  d.dart'
 
 /-- The dart with reversed orientation from a given dart. -/
 @[simps]
-def Dart.symm (d : G.Dart) : G.Dart :=
-  ⟨d.toProd.swap, G.symm d.adj⟩
+def prodDart.symm (d : prodDart G) : prodDart G :=
+  ⟨d.toProd.swap, G.symm d.dart'⟩
 
 @[simp]
-theorem Dart.symm_mk {p : V × V} (h : G.Adj p.1 p.2) : (Dart.mk p h).symm = Dart.mk p.swap h.symm :=
+theorem prodDart.symm_mk {p : V × V} (h : G.Adj p.1 p.2) :
+    (prodDart.mk p h).symm = prodDart.mk p.swap h.symm :=
   rfl
 
 @[simp]
-theorem Dart.edge_symm (d : G.Dart) : d.symm.edge = d.edge :=
+theorem prodDart.edge_symm (d : prodDart G) : d.symm.edge = d.edge :=
   Sym2.eq_swap
 
 @[simp]
-theorem Dart.edge_comp_symm : Dart.edge ∘ Dart.symm = (Dart.edge : G.Dart → Sym2 V) :=
-  funext Dart.edge_symm
+theorem prodDart.edge_comp_symm :
+    prodDart.edge ∘ prodDart.symm = (prodDart.edge : prodDart G → Sym2 V) :=
+  funext prodDart.edge_symm
 
 @[simp]
-theorem Dart.symm_symm (d : G.Dart) : d.symm.symm = d :=
-  Dart.ext _ _ <| Prod.swap_swap _
+theorem prodDart.symm_symm (d : prodDart G) : d.symm.symm = d :=
+  prodDart.ext' _ _ <| Prod.swap_swap _
 
 @[simp]
-theorem Dart.symm_involutive : Function.Involutive (Dart.symm : G.Dart → G.Dart) :=
-  Dart.symm_symm
+theorem prodDart.symm_involutive : Function.Involutive (prodDart.symm : prodDart G → prodDart G) :=
+  prodDart.symm_symm
 
-theorem Dart.symm_ne (d : G.Dart) : d.symm ≠ d :=
-  ne_of_apply_ne (Prod.snd ∘ Dart.toProd) d.adj.ne
+theorem prodDart.symm_ne (d : prodDart G) : d.symm ≠ d :=
+  ne_of_apply_ne (Prod.snd ∘ prodDart.toProd) d.dart'.ne
 
-theorem dart_edge_eq_iff : ∀ d₁ d₂ : G.Dart, d₁.edge = d₂.edge ↔ d₁ = d₂ ∨ d₁ = d₂.symm := by
+theorem dart_edge_eq_iff : ∀ d₁ d₂ : prodDart G, d₁.edge = d₂.edge ↔ d₁ = d₂ ∨ d₁ = d₂.symm := by
   rintro ⟨p, hp⟩ ⟨q, hq⟩
-  simp
+  simp only [prodDart.edge_mk, Sym2.eq, Prod.mk.eta, Sym2.rel_iff', prodDart.mk.injEq,
+    prodDart.symm_mk, Prod.fst_swap, Prod.snd_swap]
+  grind
 
 theorem dart_edge_eq_mk'_iff :
-    ∀ {d : G.Dart} {u v : V}, d.edge = s(u, v) ↔ d.toProd = (u, v) ∨ d.toProd = (v, u) := by
+    ∀ {d : prodDart G} {u v : V}, d.edge = s(u, v) ↔ d.toProd = (u, v) ∨ d.toProd = (v, u) := by
   rintro ⟨p, h⟩ _ _
   simp
 
 theorem dart_edge_eq_mk'_iff' :
-    ∀ {d : G.Dart} {u v : V},
+    ∀ {d : prodDart G} {u v : V},
       d.edge = s(u, v) ↔ d.fst = u ∧ d.snd = v ∨ d.fst = v ∧ d.snd = u := by
   rintro ⟨⟨a, b⟩, h⟩ u v
   rw [dart_edge_eq_mk'_iff]
@@ -114,26 +81,20 @@ theorem dart_edge_eq_mk'_iff' :
 
 variable (G)
 
-/-- Two darts are said to be adjacent if they could be consecutive
-darts in a walk -- that is, the first dart's second vertex is equal to
-the second dart's first vertex. -/
-def DartAdj (d d' : G.Dart) : Prop :=
-  d.snd = d'.fst
-
 /-- For a given vertex `v`, this is the bijective map from the neighbor set at `v`
 to the darts `d` with `d.fst = v`. -/
 @[simps]
-def dartOfNeighborSet (v : V) (w : G.neighborSet v) : G.Dart :=
+def dartOfNeighborSet (v : V) (w : G.neighborSet v) : prodDart G :=
   ⟨(v, w), w.property⟩
 
-theorem dartOfNeighborSet_injective (v : V) : Function.Injective (G.dartOfNeighborSet v) :=
+theorem dartOfNeighborSet_injective (v : V) : Function.Injective (dartOfNeighborSet G v) :=
   fun e₁ e₂ h =>
   Subtype.ext <| by
     injection h with h'
     convert congr_arg Prod.snd h'
 
-instance nonempty_dart_top [Nontrivial V] : Nonempty (⊤ : SimpleGraph V).Dart := by
+instance nonempty_dart_top [Nontrivial V] : Nonempty (prodDart (⊤ : SimpleGraph V)) := by
   obtain ⟨v, w, h⟩ := exists_pair_ne V
   exact ⟨⟨(v, w), h⟩⟩
 
-end SimpleGraph
+end HasDart
